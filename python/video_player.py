265558b49e26f880d0a0962d8e713947f6d3ec4c
Brian Webb <webbbn@gmail.com>
2019-12-18 16:11:19 +0000

    Fixes the location of the hello_video program and ensures that it exists before trying to run it.

--- python/video_player.py
+++ python/video_player.py
@@ -6,17 +6,27 @@ import subprocess as sp
 
 class VideoPlayer(object):
     '''Constrols the ground-side video player / OSD'''
+    video_player="/home/pi/wifibroadcast-hello_video/hello_video.bin.48-mm"
+    osd_program="/usr/local/bin/QOpenHD"
 
     def __init__(self):
         self.done = False;
-        self.video_thread = threading.Thread(target = self.start_video)
-        self.video_thread.start()
-        self.osd_thread = threading.Thread(target = self.start_osd)
-        self.osd_thread.start()
+        if os.path.isfile(self.video_player) and os.access(self.video_player, os.X_OK) and \
+           os.path.isfile(self.osd_program) and os.access(self.osd_program, os.X_OK):
+            self.video_thread = threading.Thread(target = self.start_video)
+            self.video_thread.start()
+            self.osd_thread = threading.Thread(target = self.start_osd)
+            self.osd_thread.start()
+        else:
+            self.video_thread = None
+            self.osd_thread = None
 
     def __del__(self):
         self.done = True
         self.join()
+
+    def running(self):
+        return self.video_thread or self.osd_thread
 
     def join(self):
         if self.video_thread:
@@ -26,11 +36,11 @@ class VideoPlayer(object):
 
     def start_video(self):
         nc = sp.Popen(["/bin/nc", "-l", "-u", "5600"], stdout=sp.PIPE)
-        hv = sp.Popen(["/opt/vc/src/hello_pi/hello_video/hello_video.bin.48.mm"], stdin=nc.stdout)
+        hv = sp.Popen([self.video_player], stdin=nc.stdout)
         hv.wait()
 
     def start_osd(self):
-        qopenhd = sp.run("/usr/local/bin/QOpenHD")
+        qopenhd = sp.run(self.osd_program)
 
 if __name__ == '__main__':
     vid = VideoPlayer()
